bbcc06a1d0f304048c9dc84cb91e8d9cf1b8501d
Loren Cerri <lorencerri@gmail.com>
2023-03-13 16:04:09 -0400

    Update code to 8517

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,12 @@
 import {
-    createStyles,
-    Anchor,
-    Group,
-    Title,
-    Transition,
-    Button,
-    Modal,
-    Alert,
+  createStyles,
+  Anchor,
+  Group,
+  Title,
+  Transition,
+  Button,
+  Modal,
+  Alert,
 } from "@mantine/core";
 import { useStore } from "app/state";
 import { IconAlertCircle } from "@tabler/icons";
@@ -14,135 +14,131 @@ import { IconAlertCircle } from "@tabler/icons";
 import { useEffect, useState } from "react";
 
 const useStyles = createStyles((theme) => ({
-    inner: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "16px 16px",
-        marginTop: 120,
-        borderTop: "1px solid #2C2E33",
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "16px 16px",
+    marginTop: 120,
+    borderTop: "1px solid #2C2E33",
 
-        [theme.fn.smallerThan("sm")]: {
-            flexDirection: "column",
-        },
+    [theme.fn.smallerThan("sm")]: {
+      flexDirection: "column",
+    },
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: "8px 12px",
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+      textDecoration: "none",
     },
 
-    link: {
-        display: "block",
-        lineHeight: 1,
-        padding: "8px 12px",
-        borderRadius: theme.radius.sm,
-        textDecoration: "none",
-        color:
-            theme.colorScheme === "dark"
-                ? theme.colors.dark[0]
-                : theme.colors.gray[7],
-        fontSize: theme.fontSizes.sm,
-        fontWeight: 500,
-
-        "&:hover": {
-            backgroundColor:
-                theme.colorScheme === "dark"
-                    ? theme.colors.dark[6]
-                    : theme.colors.gray[0],
-            textDecoration: "none",
-        },
-
-        [theme.fn.smallerThan("sm")]: {
-            borderRadius: 0,
-            padding: theme.spacing.md,
-        },
+    [theme.fn.smallerThan("sm")]: {
+      borderRadius: 0,
+      padding: theme.spacing.md,
     },
+  },
 }));
 
 const links = [
-    { link: "https://twitter.com/plexidev", label: "Twitter" },
-    { link: "https://youtube.com/c/truexpixels", label: "YouTube" },
-    { link: "https://github.com/plexidev", label: "GitHub" },
-    { link: "https://discord.gg/plexidev", label: "Discord" },
+  { link: "https://twitter.com/plexidev", label: "Twitter" },
+  { link: "https://youtube.com/c/truexpixels", label: "YouTube" },
+  { link: "https://github.com/plexidev", label: "GitHub" },
+  { link: "https://discord.gg/plexidev", label: "Discord" },
 ];
 
 export default function Footer() {
-    const [mounted, setMounted] = useState(false);
-    const [opened, setOpened] = useState(false);
-    const displayKeypad = useStore((state) => state.displayKeypad);
-    const { classes } = useStyles();
+  const [mounted, setMounted] = useState(false);
+  const [opened, setOpened] = useState(false);
+  const displayKeypad = useStore((state) => state.displayKeypad);
+  const { classes } = useStyles();
 
-    useEffect(() => {
-        setMounted(true);
-    }, [mounted]);
+  useEffect(() => {
+    setMounted(true);
+  }, [mounted]);
 
-    const items = links.map((link) => (
-        <Anchor<"a">
-            color="dimmed"
-            key={link.label}
-            href={link.link}
-            sx={{ lineHeight: 1 }}
-            className={classes.link}
-            size="sm"
+  const items = links.map((link) => (
+    <Anchor<"a">
+      color="dimmed"
+      key={link.label}
+      href={link.link}
+      sx={{ lineHeight: 1 }}
+      className={classes.link}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        overlayOpacity={0.55}
+        overlayBlur={3}
+        title="Contact Us"
+      >
+        <Alert
+          icon={<IconAlertCircle size={16} />}
+          title="Oops!"
+          color="yellow"
         >
-            {link.label}
-        </Anchor>
-    ));
+          This component is still under development. You can contact us by
+          joining our Discord server.
+        </Alert>
+      </Modal>
+      <Transition
+        transition="fade"
+        mounted={mounted}
+        duration={1000}
+        timingFunction="ease"
+      >
+        {(styles) => {
+          return (
+            <div className={classes.inner} style={styles}>
+              <Title order={2}>
+                {displayKeypad ? (
+                  <span>
+                    <span style={{ color: "#3b5bdb" }}>85</span>
+                    17
+                  </span>
+                ) : (
+                  <span>
+                    <span style={{ color: "#3b5bdb" }}>plexi</span>
+                    dev
+                  </span>
+                )}
+              </Title>
 
-    return (
-        <div>
-            <Modal
-                opened={opened}
-                onClose={() => setOpened(false)}
-                overlayOpacity={0.55}
-                overlayBlur={3}
-                title="Contact Us"
-            >
-                <Alert
-                    icon={<IconAlertCircle size={16} />}
-                    title="Oops!"
-                    color="yellow"
+              <Group>
+                {items}
+                <Button
+                  style={{ backgroundColor: "#3b5bdb" }}
+                  onClick={() => setOpened(true)}
                 >
-                    This component is still under development. You can contact
-                    us by joining our Discord server.
-                </Alert>
-            </Modal>
-            <Transition
-                transition="fade"
-                mounted={mounted}
-                duration={1000}
-                timingFunction="ease"
-            >
-                {(styles) => {
-                    return (
-                        <div className={classes.inner} style={styles}>
-                            <Title order={2}>
-                                {displayKeypad ? (
-                                    <span>
-                                        <span style={{ color: "#3b5bdb" }}>
-                                            32
-                                        </span>
-                                        58
-                                    </span>
-                                ) : (
-                                    <span>
-                                        <span style={{ color: "#3b5bdb" }}>
-                                            plexi
-                                        </span>
-                                        dev
-                                    </span>
-                                )}
-                            </Title>
-
-                            <Group>
-                                {items}
-                                <Button
-                                    style={{ backgroundColor: "#3b5bdb" }}
-                                    onClick={() => setOpened(true)}
-                                >
-                                    Contact Us
-                                </Button>
-                            </Group>
-                        </div>
-                    );
-                }}
-            </Transition>
-        </div>
-    );
+                  Contact Us
+                </Button>
+              </Group>
+            </div>
+          );
+        }}
+      </Transition>
+    </div>
+  );
 }
--- app/state.tsx
+++ app/state.tsx
@@ -1,37 +1,40 @@
 import create from "zustand";
 
 interface BearState {
-    displayKeypad: boolean;
-    code: string;
-    enableKeypad: () => void;
-    pressKey: (key: string | number) => void;
-    success: boolean;
+  displayKeypad: boolean;
+  code: string;
+  enableKeypad: () => void;
+  pressKey: (key: string | number) => void;
+  success: boolean;
 }
 
 export const useStore = create<BearState>()((set) => ({
-    displayKeypad: false,
-    code: "",
-    success: false,
-    enableKeypad: () =>
-        set(() => {
-            console.log(
-                "The code is in the bottom left corner of the webpage if you're having trouble finding it!"
-            );
-            return { displayKeypad: true };
-        }),
-    pressKey: (key) => {
-        set((state) => {
-            switch (key) {
-                case "Back":
-                    if (state.code.length === 0) return { code: state.code };
-                    else return { code: state.code.slice(0, -1) };
-                case "Enter":
-                    if (state.code === "3258") return { success: true };
-                    else return { code: "" };
-                default:
-                    if (state.code.length === 4) return { code: state.code };
-                    return { code: state.code + key };
-            }
-        });
-    },
+  displayKeypad: false,
+  code: "",
+  success: false,
+  enableKeypad: () =>
+    set(() => {
+      console.log(
+        "The code is in the bottom left corner of the webpage if you're having trouble finding it!"
+      );
+      return { displayKeypad: true };
+    }),
+  pressKey: (key) => {
+    set((state) => {
+      switch (key) {
+        case "Back":
+          if (state.code.length === 0) return { code: state.code };
+          else return { code: state.code.slice(0, -1) };
+        case "Enter":
+          if (state.code === "8517")
+            return {
+              success: true, // 8/5/17 -> August 5th, 2017 (Start of Plexi)
+            };
+          else return { code: "" };
+        default:
+          if (state.code.length === 4) return { code: state.code };
+          return { code: state.code + key };
+      }
+    });
+  },
 }));
